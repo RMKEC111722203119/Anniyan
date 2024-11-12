0139e2c52bbcb39654ff8441f0996e50369402a3
Vigneshwaran <119487323+RMKEC111722203119@users.noreply.github.com>
2024-11-12 07:59:45 +0530

    Update login.js

--- anniyan/src/components/login.js
+++ anniyan/src/components/login.js
@@ -13,7 +13,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('https://vercel.live/link/backend-git-main-rmkec111722203119s-projects.vercel.app?via=deployment-domains-list&p=1/', { email, password },
+      const response = await axios.post('https://anniyan2-i3a7x83ev-rmkec111722203119s-projects.vercel.app/', { email, password },
         {
           headers: {
             'Access-Control-Allow-Origin': '*',
