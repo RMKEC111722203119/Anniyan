30f4eb8dc060c3d3523a510dc8eb961143d2caaa
Vigneshwaran <119487323+RMKEC111722203119@users.noreply.github.com>
2025-01-17 13:23:02 +0530

    Update register.js

--- anniyan/src/components/register.js
+++ anniyan/src/components/register.js
@@ -13,7 +13,7 @@ const Register = () => {
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('https://anniyan2-ojgyk6fn9-rmkec111722203119s-projects.vercel.app/register', { email, password },
+      const response = await axios.post('https://anniyan-1.onrender.com/register', { email, password },
         {
           headers: {
             'Access-Control-Allow-Origin': '*',
