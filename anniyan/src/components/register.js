dbde2f25e81544af1fe93dbced2d66fb16532208
Vigneshwaran <119487323+RMKEC111722203119@users.noreply.github.com>
2025-01-22 18:09:02 +0530

    Update register.js

--- anniyan/src/components/register.js
+++ anniyan/src/components/register.js
@@ -13,7 +13,7 @@ const Register = () => {
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('https://anniyan-1.onrender.com/register', { email, password },
+      const response = await axios.post('https://anniyan-97a3.onrender.com/register', { email, password },
         {
           headers: {
             'Access-Control-Allow-Origin': '*',
