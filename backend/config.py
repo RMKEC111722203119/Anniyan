21d4a679e8d8350a1ab5c8f4e8f646cf05b4f736
Vigneshwaran <119487323+RMKEC111722203119@users.noreply.github.com>
2025-04-01 16:30:19 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -6,7 +6,7 @@ class ApplicationConfig:
     # Email configuration
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
-    MAIL_USERNAME = 'vign22112.it@rmkec.ac.in'
+    MAIL_USERNAME = ''
     MAIL_PASSWORD = 'azcm lcuy exnq ffmo'
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
